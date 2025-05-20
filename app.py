2ea9bcee7b95d16929d518a0a93c127a911444a4
Jarsenal <43666111+Jarsenal@users.noreply.github.com>
2025-05-20 23:30:47 +1000

    Update app.py

--- app.py
+++ app.py
@@ -4,5 +4,6 @@ app = Flask(__name__)
 @app.route("/")
 def hello():
     return "<h1>Hello, World! This is Monkey JayJay</h1>"
+@app.route("/index")
 def index():
     return "<h1>Hello, World! This is index</h1>"
