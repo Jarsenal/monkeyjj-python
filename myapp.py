02accb34c4daadabc29c543ad6fe0f74d1caa912
Jarsenal <43666111+Jarsenal@users.noreply.github.com>
2025-05-22 20:25:11 +1000

    Update and rename app.py to myapp.py

--- myapp.py
+++ myapp.py
@@ -1,18 +1,18 @@
 from flask import Flask
-app = Flask(__name__)
+myapp = Flask(__name__)
 
 from flask import render_template
 
-@app.route("/")
+@myapp.route("/")
 def hello():
     return "<h1>Hello, World! This is Monkey JayJay</h1>"
     
-@app.route("/index.html")
-@app.route("/fake.html")
+@myapp.route("/index.html")
+@myapp.route("/fake.html")
 def indexG():
     return "<h1>Hello, World! This is index.html</h1>"
 
 username = 'daddyyyy'
-@app.route("/" + username)
+@myapp.route("/" + username)
 def name():
     return render_template('index.html', username=username)
