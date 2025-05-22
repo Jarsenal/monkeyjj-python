dbae5c099ca2400df985bd0f33fc801ef940468e
Jarsenal <43666111+Jarsenal@users.noreply.github.com>
2025-05-22 20:32:50 +1000

    Update app.py

--- app.py
+++ app.py
@@ -1,18 +1,18 @@
 from flask import Flask
-myapp = Flask(__name__)
+app = Flask(__name__)
 
 from flask import render_template
 
-@myapp.route("/")
+@app.route("/")
 def hello():
     return "<h1>Hello, World! This is Monkey JayJay</h1>"
     
-@myapp.route("/index.html")
-@myapp.route("/fake.html")
+@app.route("/index.html")
+@app.route("/fake.html")
 def indexG():
     return "<h1>Hello, World! This is index.html</h1>"
 
 username = 'daddyyyy'
-@myapp.route("/" + username)
+@app.route("/" + username)
 def name():
     return render_template('index.html', username=username)
